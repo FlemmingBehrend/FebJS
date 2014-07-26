9bca70538e83bfe6ebffd766a6b33170cfea47b1
FlemmingBehrend <vakser99>
2014-07-26 22:16:16 +0200

    Parsing true, false, and null

--- src/Parse.js
+++ src/Parse.js
@@ -1,6 +1,8 @@
 /* jshint globalstrict: true */
 'use strict';
 
+var ESCAPES = {"n":"\n", "f":"\f", "r":"\r", "t":"\t", "v":"\v", "'":"'", "\"":"\""};
+
 function parse(expr) {
     var lexer = new Lexer();
     var parser = new Parser(lexer);
@@ -34,10 +36,28 @@ Lexer.prototype.readString = function (quote) {
     this.index++;
     var rawString = quote;
     var string = '';
+    var escape = false;
     while (this.index < this.text.length) {
         var ch = this.text.charAt(this.index);
         rawString += ch;
-        if (ch === quote) {
+        if (escape) {
+            if (ch === "u") {
+                var hex = this.text.substring(this.index + 1, this.index + 5);
+                if (!hex.match(/[\da-f]{4}/i)) {
+                    throw "Invalid unicode escape";
+                }
+                this.index += 4;
+                string = String.fromCharCode(parseInt(hex, 16));
+            } else {
+                var replacement = ESCAPES[ch];
+                if (replacement) {
+                    string += replacement;
+                } else {
+                    string += ch;
+                }
+            }
+            escape = false;
+        } else if (ch === quote) {
             this.index++;
             this.tokens.push({
                 text: rawString,
@@ -45,6 +65,8 @@ Lexer.prototype.readString = function (quote) {
                 fn: _.constant(string)
             });
             return;
+        } else if (ch === '\\') {
+            escape = true;
         } else {
             string += ch;
         }
