a8d3aa1f7628af7b57766a1b4821f99e9af4a86a
Flemming Behrend <flemmingjaeger@gmail.com>
2015-03-05 20:29:57 +0100

    Requiring Other Modules

--- test/injecterTest.js
+++ test/injecterTest.js
@@ -1,5 +1,5 @@
 /* jshint globalstrict: true */
-
+/* global angular: false */
 "use strict";
 
 describe('injector', function () {
@@ -12,6 +12,26 @@ describe('injector', function () {
     it ('Should be possible to create an injector', function () {
         var injector = createInjector([]);
         expect(injector).toBeDefined();
+    });
+
+    it ('Should have a constant that that have been registered to a module', function () {
+        var module = angular.module('myModule', []);
+        module.constant('aConstant', 42);
+        var injector = createInjector(['myModule']);
+        expect(injector.has('aConstant')).toBe(true);
+    });
+
+    it ('Should not have non-registered constant', function () {
+        angular.module('myModule', []);
+        var injector = createInjector(['myModule']);
+        expect(injector.has('aConstant')).toBe(false);
+    });
+
+    it ('Should be able to return a registered constant', function () {
+        var module = angular.module('myModule', []);
+        module.constant('aConstant', 42);
+        var injector = createInjector(['myModule']);
+        expect(injector.get('aConstant')).toBe(42);
     });
 
 });
