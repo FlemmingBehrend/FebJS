1876fdd962db6b425648dcd06ea03596c58bd646
Flemming Behrend <flemmingjaeger@gmail.com>
2015-03-18 16:59:07 +0100

    Dependency Injection

--- test/injecterTest.js
+++ test/injecterTest.js
@@ -34,4 +34,42 @@ describe('injector', function () {
         expect(injector.get('aConstant')).toBe(42);
     });
 
+    it ('Should be able to load multiple modules', function () {
+        var module1 = angular.module('myModule1', []);
+        var module2 = angular.module('myModule2', []);
+        module1.constant('aConstant', 42);
+        module2.constant('anotherConstant', 43);
+        var injector = createInjector(['myModule1', 'myModule2']);
+        expect(injector.has('aConstant')).toBe(true);
+        expect(injector.has('anotherConstant')).toBe(true);
+    });
+
+    it ('Should load the required modules of another module', function () {
+        var module1 = angular.module('myModule1', []);
+        var module2 = angular.module('myModule2', ['myModule1']);
+        module1.constant('aConstant', 42);
+        module2.constant('anotherConstant', 43);
+        var injector = createInjector(['myModule2']);
+        expect(injector.has('aConstant')).toBe(true);
+        expect(injector.has('anotherConstant')).toBe(true);
+    });
+
+    it ('Should load required modules recursively', function () {
+        var module1 = angular.module('myModule1', []);
+        var module2 = angular.module('myModule2', ['myModule1']);
+        var module3 = angular.module('myModule3', ['myModule2']);
+        module1.constant('aConstant', 42);
+        module2.constant('anotherConstant', 43);
+        module3.constant('yetAnotherConstant', 44);
+        var injector = createInjector(['myModule3']);
+        expect(injector.has('aConstant')).toBe(true);
+        expect(injector.has('anotherConstant')).toBe(true);
+        expect(injector.has('yetAnotherConstant')).toBe(true);
+    });
+
+    it ('Should load each module only once', function () {
+        angular.module('myModule1', ['myModule2']);
+        angular.module('myModule2', ['myModule1']);
+        createInjector(['myModule1', 'myModule2']);
+    });
 });
